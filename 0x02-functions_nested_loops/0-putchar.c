2ddb840b7336c20af2692aa5591b89cf35531af4
github-lqdsana <s.louqid@gmail.com>
2023-09-14 06:21:18 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
-
 /**
-*main - a program that prints _putchar
-*
-*Return:return 0
-*/
+ * main - print _putchar
+ * a program that prints _putchar
+ * Return: 0
+ */
+
 int main(void)
 {
-	char school[10] = "_putchar";
-
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(school[i]);
-	}
-	_putchar(10);
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
