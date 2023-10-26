9acf198d839d96d5ed0b1255a6fd55cd2c640e05
lqdsana <s.louqid@gmail.com>
2023-10-26 11:43:21 -0700

    a

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -2,10 +2,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print 
+ * @c:the character to print 
  *
- * Return: On success 1. 
- * On error, -1 is returned, 
+ * Return:On success 1. 
+ * On error,-1 is returned, 
  */
 int _putchar(char c)
 {
