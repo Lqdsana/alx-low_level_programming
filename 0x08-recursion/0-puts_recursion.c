8c9b06ba9d9cefec45b80a409e7434ae2ef0238e
lqdsana <s.louqid@gmail.com>
2023-10-26 10:47:28 -0700

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,18 @@
 /**
  * _puts_recursion - function like puts();
  * @s: input
- * Return: Always 0 (Success)
+ * Return: void (Success)
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s== '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
