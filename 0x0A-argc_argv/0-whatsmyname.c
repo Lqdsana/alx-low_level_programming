5c4eabc5758c317bdc8c0ac5ca4b4a059fafd338
lqdsana <s.louqid@gmail.com>
2023-11-01 12:20:31 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ * prints the name of the program, including the path.
+ * Return: Always 0
+ */
+int main(int argc, char *argv[]) 
+{
+	(void)argc;
+
+	printf("%s\n", argv[]);
+	return (0);
+}
