1d4e36aac7f08d119ed19d4dc61293b3160b609a
lqdsana <s.louqid@gmail.com>
2023-11-03 11:16:31 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main-print the result of the multiplication, followed by a new line.
+ * @argc:number of command-line arguments.
+ * @argv:pointer to an array of command-line arguments.
+ * Return:0-success.
+ */
+
+int main(int argc, char *argv[])
+{
+int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
