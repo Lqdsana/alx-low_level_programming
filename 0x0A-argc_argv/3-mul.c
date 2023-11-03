03c579731384c672131e4329c5a6c21bdafd2e73
lqdsana <s.louqid@gmail.com>
2023-11-03 09:48:03 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib>
 
 /**
- * main - Entry point
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- *
- * Description: Multiplies two numbers and prints the result.
- * Return: 0 for success, 1 for error.
+ * main-print the result of the multiplication, followed by a new line.
+ * @argc:number of command-line arguments.
+ * @argv:pointer to an array of command-line arguments.
+ * Return:0-success.
  */
+
 int main(int argc, char *argv[])
 {
-int sum;
+	int sum;
 
-if (argc == 3)
-{
-sum = atoi(argv[1]) *  atoi(argv[2])
-printf("%d\n", sum);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2])
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
